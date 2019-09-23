2a3b9f6b4f0247da1039e4537dc06961c1277fba
Luis Felipe <rl.luisfe@gmail.com>
2019-09-23 16:22:52 -0400

    create entities

---
+++ BooksAPI.Entities/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksAPI.Entities.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Year { get; set; }
+        public Genre Genre { get; set; }
+        public Author Author { get; set; }
+    }
+}
