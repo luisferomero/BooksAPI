523331b8c36df9db36fa97400823c77254979900
Luis Felipe <rl.luisfe@gmail.com>
2019-09-25 16:48:07 -0400

    generic repository

---
+++ BooksAPI.Persistence/Repositories/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksAPI.Persistence.Repositories
+{
+    public interface IGenericRepository
+    {
+        public interface IGenericRepository<TEntity> where TEntity : class
+        {
+            TEntity GetById(int Id);
+            IEnumerable<TEntity> GetAll();
+            IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+            TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+            void Add(TEntity entity);
+            void AddRange(IEnumerable<TEntity> entities);
+            void Remove(TEntity entity);
+            void RemoveRange(IEnumerable<TEntity> entities);
+            void Update(TEntity entity);
+            IEnumerable<TEntity> GetListMultipleInclude(params Expression<Func<TEntity, object>>[] includes);
+            TEntity GetSingleMultipleInclude(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
+            Task<TEntity> GetIncludeAsync(Expression<Func<TEntity, object>> include, Expression<Func<TEntity, bool>> predicate);
+            int GetQuantity();
+            bool Any(Expression<Func<TEntity, bool>> predicate);
+        }
+    }
+}
